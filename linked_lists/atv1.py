0406e1d02caaf872e3d54008d3ffef996573f4a4
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-08-15 11:39:13 -0300

    Update atv1.py

--- linked_lists/atv1.py
+++ linked_lists/atv1.py
@@ -21,41 +21,63 @@ class List:
             print(current.content)
             current = current.next
 
-    def append(self,x):
-        new = Node(x,None)
+    def append(self,data):
+        new = Node(data,None)
         self.end.next = new
         self.end = new
 
-    def search(self,x):
+    def search(self,data):
        current = self.head.next 
        while(current.content != None):
-        if current.content == x:
+        if current.content == data:
             return True
         current = current.next
         return False
     
-    def remove(self,x):
+    def remove(self,data):
         current = self.head.next
         previous = self.head
         while(current != None):
-            if (current.content == x):
+            if (current.content == data):
                 previous.next = current.next
                 current.next = None
             previous = current
             current = current.next
-        if(self.end.content == x):
+        if(self.end.content == data):
             self.end = previous
     
-    def index(self,x):
+    def index(self,data):
         index = 0
         current = self.head.next
-        previous = self.head
         while(current != None):
-            if(current.content == x):
+            if(current.content == data):
                 return index
             else: 
                 index = index + 1
                 current = current.next
-                previous = current
-        if(self.end.content != x):
-            print('número inexistente')
+        if(self.end.content != data):
+            return None
+    
+    def clear(self):
+        current = self.head.next
+        while(current != None):
+            current.content = None
+            current = current.next
+    
+    def count(self, data):
+        count = 0
+        current = self.head.next
+        while(current != None):
+            if(current.content == data):
+                current = current.next
+                count = count + 1
+            elif(current.content != data):
+                current = current.next
+        return count
+
+list = List()
+list.append(1)
+list.append(1)
+list.append(1)
+list.append(2)
+print(list.count(1))
