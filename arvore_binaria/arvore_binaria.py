80edabae5d61ba92dbf2db897350d0b339ee265e
1917dc <l19051997@gmail.com>
2024-10-29 19:43:46 -0300

    Implementação de árvore binária de busca

--- arvore_binaria/arvore_binaria.py
+++ arvore_binaria/arvore_binaria.py
@@ -1,14 +1,40 @@
-# fazer a classe Node definindo as partes da esquerda e direita do nó
+# fazer um algoritmo de busca de árvore binária
+
+
+#definir a node que será usada na árvore
 
 class Node:
     def __init__(self, value):
+
         self.key = value
 
-        # definindo as pontas da árvore
         self.left = None
         self.right = None
 
-class Tree:
-    def __init__(self):
-        self.root = None
-        
+    def insert(self, value):
+        if(self.key == None):
+           self.key = value
+
+        if(self.key == value):
+            return
+
+        if(self.key < value):
+            if(self.right == None):
+                self.right = Node(value)
+
+            else:
+                cmp = self.right
+                while(cmp.key != None):
+                    if(cmp.key < value):
+                        cmp = cmp.right
+
+                    elif(cmp.key > value):
+                        cmp.left = Node(value)
+                    
+                self.right = Node(value)
+
+node = Node(2)
+print(node.key)
+node.insert(3)
+print(node.right.key)
+node.insert(4)
