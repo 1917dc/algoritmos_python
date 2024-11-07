67be86fac5623a7b965968117250b73bdc552f79
1917dc <l19051997@gmail.com>
2024-11-07 19:46:53 -0300

    Implementando métodos em árvore binária de busca

--- arvore_binaria/arvore_binaria.py
+++ arvore_binaria/arvore_binaria.py
@@ -1,40 +1,69 @@
-# fazer um algoritmo de busca de árvore binária
-
-
-#definir a node que será usada na árvore
-
 class Node:
     def __init__(self, value):
-
         self.key = value
-
         self.left = None
         self.right = None
 
     def insert(self, value):
-        if(self.key == None):
-           self.key = value
-
-        if(self.key == value):
+        if not self.key:
+            self.key = value
+        
+        if self.key == value:
+            return
+        
+        if self.key < value:
+            if self.right:
+                self.right.insert(value)
+                return
+            self.right = Node(value)
             return
 
-        if(self.key < value):
-            if(self.right == None):
-                self.right = Node(value)
+        if self.key > value:
+            if self.left:
+                self.left.insert(value)
+                return
+            self.left = Node(value)
+            return
 
-            else:
-                cmp = self.right
-                while(cmp.key != None):
-                    if(cmp.key < value):
-                        cmp = cmp.right
+    def exists(self, value):
+        if self.key is value:
+            return True
+        
+        if self.key < value:
+            if self.right is None:
+                return False
 
-                    elif(cmp.key > value):
-                        cmp.left = Node(value)
-                    
-                self.right = Node(value)
+            return self.right.exists(value)
+        
+        if self.key > value:
+            if self.left is None:
+                return False
 
-node = Node(2)
-print(node.key)
+            return self.left.exists(value)
+
+    def delete(self, value):
+        if not self.exists(value):
+            return False
+        
+        
+        
+
+    def get_min(self):
+        current = self
+        while current.left != None:
+            current = current.left
+        return current.key
+    
+    def get_max(self):
+        current = self
+        while current.right != None:
+            current = current.right
+        return current.key
+ 
+node = Node(4)
+
+node.insert(2)
 node.insert(3)
-print(node.right.key)
-node.insert(4)
+node.insert(5)
+node.insert(7)
+node.insert(1)
